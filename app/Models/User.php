169e483cffd911949744b502e8836235ec4bbdb7
Sarmad Gardezi <mail@sarmadgardezi.com>
2026-01-07 23:20:33 +0500

    push

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,31 +2,31 @@
 
 namespace App\Models;
 
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
+    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
      *
-     * @var array<int, string>
+     * @var list<string>
      */
     protected $fillable = [
         'name',
         'email',
         'password',
-        'role',
-        'status',
     ];
 
     /**
      * The attributes that should be hidden for serialization.
      *
-     * @var array<int, string>
+     * @var list<string>
      */
     protected $hidden = [
         'password',
@@ -44,25 +44,5 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
-    }
-
-    public function isAdmin(): bool
-    {
-        return $this->role === 'admin';
-    }
-
-    public function isVendor(): bool
-    {
-        return $this->role === 'vendor';
-    }
-
-    public function products()
-    {
-        return $this->hasMany(Product::class);
-    }
-    
-    public function subscriptions()
-    {
-        return $this->hasMany(Subscription::class);
     }
 }
